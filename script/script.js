1e20e82047c15cc72b3fcb9e12c1410295b1428e
Alexey Konechnny <a.konechnyy93@gmail.com>
2024-06-17 14:59:38 +0300

    Сделана разметка следующий блоков: отзывы, клинеты, числа, последние статьи

--- script/script.js
+++ script/script.js
@@ -6,3 +6,17 @@ function openModal() {
 function closeModal() {
     document.getElementById("overlay").style.top = "-100vh";
 }
+
+//Swiper
+
+var swiper = new Swiper(".feedback-swiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+    }
+});
