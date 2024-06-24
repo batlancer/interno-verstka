83f04e5aee60af92d0f8ddcd59a12788c150bca6
9akuStoV <200pkm9@gmail.com>
2024-06-24 18:28:28 +0300

    Доделал страниу портфолио

--- script/script.js
+++ script/script.js
@@ -1,18 +1,41 @@
 //Search
 
 function openModal() {
-    document.getElementById("overlay").style.top = "0px";
+    document.getElementById('overlay').style.top = '0px';
 }
 function closeModal() {
-    document.getElementById("overlay").style.top = "-100vh";
+    document.getElementById('overlay').style.top = '-100vh';
+}
+
+// Tabs
+
+function openTab(event, tabName) {
+    var i, tabContent, tabBtns;
+
+    // Скрываем все элементы с классом 'tab-content'
+    tabContent = document.getElementsByClassName('tab-content');
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = 'none';
+    }
+
+    // Удаляем класс 'active' со всех кнопок с классом 'tab-btn'
+    tabBtns = document.getElementsByClassName('tab-btn');
+    for (i = 0; i < tabBtns.length; i++) {
+        tabBtns[i].classList.remove('active');
+    }
+
+    // Отображаем текущую вкладку и добавляем класс 'active' к кнопке 
+    document.getElementById(tabName).style.display = 'block';
+    event.currentTarget.classList.add('active');
+
 }
 
 //Swiper
 
-var swiper = new Swiper(".feedback-swiper", {
+var swiper = new Swiper('.feedback-swiper', {
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     effect: 'coverflow',
     coverflowEffect: {
